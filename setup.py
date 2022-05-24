2c2bf02e22a472baa39d3138700df8cb8420ed71
Claudia Chu <cchu@broadinstitute.org>
2022-05-24 11:48:10 -0400

    distribution package

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="JupyterReviewer-cchu70",
+    version="0.0.1",
+    author="Claudia Chu",
+    author_email="cchu@broadinstitute.org",
+    description="A general tool to create dashboards for manual review",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/getzlab/JupyterReviewer",
+    project_urls={
+        "Bug Tracker": "https://github.com/getzlab/JupyterReviewer/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "jupyter_reviewer"},
+    packages=setuptools.find_packages(where="jupyter_reviewer"),
+    python_requires=">=3.6",
+)   
+
