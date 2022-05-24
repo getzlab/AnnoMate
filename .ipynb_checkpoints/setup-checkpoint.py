825e6a2f5a978462fcab9afb4be9203b82483e9c
Claudia Chu <cchu@broadinstitute.org>
2022-05-24 11:52:30 -0400

    change pip install package name

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="JupyterReviewer-cchu70",
+    name="jupyter_reviewer",
     version="0.0.1",
     author="Claudia Chu",
     author_email="cchu@broadinstitute.org",
