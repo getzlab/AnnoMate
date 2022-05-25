710a9239c28afbc8f9857e3bf3a5dddd18b57977
Claudia Chu <cchu@broadinstitute.org>
2022-05-25 00:30:40 -0400

    try src restructure for setup

--- .ipynb_checkpoints/setup-checkpoint.py
+++ .ipynb_checkpoints/setup-checkpoint.py
@@ -20,8 +20,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "JupyterReviewer"},
+    packages=setuptools.find_packages(where="JupyterReviewer"),
     python_requires=">=3.6",
 )   
 
