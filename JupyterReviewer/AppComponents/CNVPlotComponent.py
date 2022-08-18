b9c6fdca66261f43bd75241ea5c6fb0a1829db8f
Conor Messer <conorsmesser@gmail.com>
2022-08-18 18:16:00 -0400

    Update preload functions to save modified mafs and seg_dfs. Use new acr_interactive interface from cnv_suite. Allow for multiple cnv plots.

--- JupyterReviewer/AppComponents/CNVPlotComponent.py
+++ JupyterReviewer/AppComponents/CNVPlotComponent.py
@@ -8,22 +8,18 @@ Mutation scatter interactive with mutation table
 
 import pandas as pd
 import numpy as np
-from dash import dcc
-from dash import html
+from dash import dcc, html
 from dash.dependencies import Input, Output, State
-import dash
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
 from scipy.stats import beta
 import pickle
 
 from JupyterReviewer.ReviewDataApp import AppComponent
 from JupyterReviewer.AppComponents.utils import cluster_color, get_unique_identifier
 
-import cnv_suite
-from cnv_suite.visualize import plot_acr_interactive, update_cnv_scatter_cn, update_cnv_scatter_color, update_cnv_scatter_sigma_toggle, calc_color, get_phylogic_color_scale
-from cnv_suite.utils import calc_avg_cn, calc_absolute_cn, calc_cn_levels, return_seg_data_at_loci, apply_segment_data_to_df, get_segment_interval_trees, switch_contigs
+from cnv_suite.visualize import plot_acr_subplots, update_cnv_color_absolute, update_cnv_scatter_sigma_toggle, plot_acr_interactive
+from cnv_suite.utils import calc_cn_levels, apply_segment_data_to_df, get_segment_interval_trees, switch_contigs
 from JupyterReviewer.DataTypes.PatientSampleData import PatientSampleData
 
 
@@ -36,7 +32,7 @@ def gen_cnv_plot_app_component():
             Input('sample-selection-checklist', 'value'),
             Input('sigma_checklist', 'value'),
             Input('cnv-color-radioitem', 'value'),
-            Input('absolute-cnv-box', 'value'),
+            Input('absolute-cnv-box', 'value'),  # todo implement as switch, not checkbox
             Input('cnv-button', 'n_clicks'),
             Input('cnv_plot', 'figure'),
             Input('sample-selection-checklist', 'options')
@@ -86,8 +82,8 @@ def gen_cnv_plot_layout():
                 html.H5('Colors:'),
                 dcc.RadioItems(
                     id='cnv-color-radioitem',
-                    options=['Differential', 'Cluster', 'Red/Blue', 'Clonal/Subclonal'],
-                    value='Differential',
+                    options=['Blue/Red', 'Difference', 'Cluster', 'Clonal/Subclonal', 'Black'],
+                    value='Difference',
                     labelStyle={'display': 'block'}
                 ),
                 html.P(''),
@@ -170,28 +166,95 @@ def gen_mut_scatter(maf_df, mut_sigma, sample):
 
     return mut_scatter
 
-def gen_preloaded_cnv_plot(samples_df, sample):
-    """Generate a CNV Plot to be stored in a pickle files
+
+def gen_preloaded_cnv_plot(participant_df, participant_id, samples_df, output_dir):
+    """Generate a CNV Plot to be stored in a pickle file
+
     Parameters
     ----------
+    participant_df: pd.DataFrame
+        Participant level DataFrame
+    participant_id: str
+        name of the participant
     samples_df: pd.DataFrame
         Sample level DataFrame
-    sample: str
-        name of the sample to be plotted
+    output_dir: str
+        Directory in which to save pre-computed pickle files
 
     Returns
     -------
-    cnv_plot : make_subplots()
-    start_trace: int
-    end_trace: int
-
     """
-    cnv_seg_df = pd.read_csv(samples_df.loc[sample, 'cnv_seg_fn'], sep='\t')
+    sample_id_list = []
+    cnv_seg_list = []
+    sample_cnv_series = pd.Series()
+    for sample_id in samples_df[samples_df['participant_id'] == participant_id].index:
+        cnv_seg_df = pd.read_csv(samples_df.loc[sample_id, 'cnv_seg_fn'], sep='\t')
+        cnv_plot, cnv_seg_df_mod, start_trace, end_trace = plot_acr_interactive(cnv_seg_df, csize, purity=samples_df.loc[sample_id, 'wxs_purity'], ploidy=samples_df.loc[sample_id, 'wxs_ploidy'])
 
-    cnv_plot = make_subplots()
-    start_trace, end_trace = plot_acr_interactive(cnv_seg_df, cnv_plot, csize, segment_colors='difference', sigmas=True)
+        output_fn = f'{output_dir}/cnv_figs/{sample_id}.cnv_fig.pkl'
+        pickle.dump([cnv_plot, cnv_seg_df_mod, start_trace, end_trace], open(output_fn, 'wb'))
+        sample_id_list.append(sample_id)
+        cnv_seg_list.append(cnv_seg_df_mod)
+        sample_cnv_series.loc[sample_id] = output_fn
 
-    return [cnv_plot, start_trace, end_trace]
+    participant_maf_series = gen_preloaded_maf(participant_df, participant_id, samples_df, cnv_seg_list, sample_id_list,
+                                               output_dir)
+
+    return sample_cnv_series, participant_maf_series
+
+
+def gen_preloaded_maf(participant_df, participant_id, sample_df, cnv_segs, sample_ids, output_dir):
+    maf_df = pd.read_csv(participant_df.loc[participant_id, 'maf_fn'], sep='\t')
+    start_pos = maf_df.columns[maf_df.columns.isin(['Start_position', 'Start_Position'])][0]
+    alt = maf_df.columns[maf_df.columns.isin(['Tumor_Seq_Allele2', 'Tumor_Seq_Allele'])][0]
+    sample_id_col = maf_df.columns[maf_df.columns.isin(['Tumor_Sample_Barcode', 'Sample_ID', 'sample_id', 'Sample_id'])][0]
+    maf_df['id'] = maf_df.apply(lambda x: get_unique_identifier(x, start_pos=start_pos, alt=alt), axis=1)
+
+    maf_df['Sample_ID'] = maf_df[sample_id_col]
+    # maf_df = switch_contigs(maf_df)
+    # .replace giving: Series.replace cannot use dict-like to_replace and non-None value ?? todo
+    maf_df['Chromosome'] = maf_df['Chromosome'].apply(lambda x: '23' if x == 'X' else '24' if x == 'Y' else x)
+
+    for i, seg_df in enumerate(cnv_segs):
+        seg_df['Sample_ID'] = sample_ids[i]
+
+    seg_trees = get_segment_interval_trees(pd.concat(cnv_segs))
+    maf_df = apply_segment_data_to_df(maf_df, seg_trees)
+    maf_df.set_index('id', inplace=True, drop=False)
+
+    purity = sample_df['wxs_purity'].astype(float).to_dict()
+    ploidy = sample_df['wxs_ploidy'].astype(float).to_dict()
+    c_values = [calc_cn_levels(pur, plo) for pur, plo in zip(purity.values(), ploidy.values())]
+    c_0 = {sample: c[0] for sample, c in zip(purity.keys(), c_values)}
+    c_delta = {sample: c[1] for sample, c in zip(purity.keys(), c_values)}
+
+    maf_df['purity'] = maf_df['Sample_ID'].replace(purity)
+    maf_df['ploidy'] = maf_df['Sample_ID'].replace(ploidy)
+    maf_df['c_0'] = maf_df['Sample_ID'].replace(c_0)
+    maf_df['c_delta'] = maf_df['Sample_ID'].replace(c_delta)
+
+    maf_df['Chromosome'] = maf_df['Chromosome'].astype(int)
+    if 'Cluster_Assignment' in maf_df:
+        maf_df['cluster_color'] = maf_df['Cluster_Assignment'].astype(int).apply(lambda x: cluster_color(x))
+
+    c_size_cumsum = np.cumsum([0] + list(csize.values()))
+    maf_df['x_loc'] = maf_df.apply(lambda x: c_size_cumsum[x['Chromosome'] - 1] + x['Start_position'], axis=1)
+    maf_df['VAF'] = maf_df['t_alt_count'] / (maf_df['t_alt_count'] + maf_df['t_ref_count'])
+
+    maf_df['mu_major_adj'] = (maf_df['mu_major'] - maf_df['c_0']) / maf_df['c_delta']
+    maf_df['mu_minor_adj'] = (maf_df['mu_minor'] - maf_df['c_0']) / maf_df['c_delta']
+    maf_df['multiplicity_ccf'] = maf_df.apply(
+        lambda x: x.VAF * (x['purity'] * (x.mu_major_adj + x.mu_minor_adj) + 2 * (1 - x['purity'])) / x['purity'], axis=1)
+    # calculate error bars for mutations
+    maf_df['error_top'] = maf_df.apply(
+        lambda x: calculate_error(x.t_alt_count, x.t_ref_count, x['purity'], 0.975), axis=1)
+    maf_df['error_bottom'] = maf_df.apply(
+        lambda x: -1 * calculate_error(x.t_alt_count, x.t_ref_count, x['purity'], 0.025), axis=1)
+
+    output_fn = f'{output_dir}/maf_df/{participant_id}.maf_df.pkl'
+    pickle.dump(maf_df, open(output_fn, 'wb'))
+
+    return pd.Series(output_fn, [participant_id])
 
 
 def gen_cnv_plot(df, idx, sample_selection, sigmas, color, absolute, selected_mutation_rows, filtered_mutation_rows, samples_df, preprocess_data_dir):
@@ -230,110 +293,39 @@ def gen_cnv_plot(df, idx, sample_selection, sigmas, color, absolute, selected_mu
     Notes
     -----
     No mutation scatter plot if no purity and ploidy in data
-
     """
+    sample_list = samples_df[samples_df['participant_id'] == idx].index.tolist()
+    # restrict sample selection to only two samples at a time
+    sample_selection_corrected = [sample_list[0]] if sample_selection == [] else sample_selection[:2]
 
-    maf_df = pd.read_csv(df.loc[idx, 'maf_fn'], sep='\t')
-    start_pos = maf_df.columns[maf_df.columns.isin(['Start_position', 'Start_Position'])][0]
-    alt = maf_df.columns[maf_df.columns.isin(['Tumor_Seq_Allele2', 'Tumor_Seq_Allele'])][0]
-    sample_id_col = maf_df.columns[maf_df.columns.isin(['Tumor_Sample_Barcode', 'Sample_ID', 'sample_id', 'Sample_id'])][0]
-    maf_df['id'] = maf_df.apply(lambda x: get_unique_identifier(x, start_pos=start_pos, alt=alt), axis=1)
-    maf_df.set_index('id', inplace=True, drop=False)
+    sigmas_val = 'Show CNV Sigmas' in sigmas
+    absolute_val = 'Display Absolute CN' in absolute
 
+    # collect Figures in list
+    fig_list = []
+    for i, sample_id in enumerate(sample_selection_corrected):
+        cnv_plot, this_seg_df, start_trace, end_trace = pickle.load(
+            open(samples_df.loc[sample_id, 'cnv_fig_pickle'], "rb"))
+        update_cnv_color_absolute(cnv_plot, this_seg_df, absolute_val, color, start_trace, end_trace)
+
+        fig_list.append(cnv_plot)
+
+    cnv_subplots_fig = plot_acr_subplots(fig_list, 'Copy Number Plots', sample_selection_corrected)
+    update_cnv_scatter_sigma_toggle(cnv_subplots_fig, sigmas_val)
+
+    maf_df = pickle.load(open(df.loc[idx, 'maf_df_pickle'], "rb"))
     if selected_mutation_rows:
         maf_df = maf_df.loc[selected_mutation_rows]
     elif filtered_mutation_rows:
         maf_df = maf_df.loc[filtered_mutation_rows]
     # else (if all mutations in table are filtered out and none selected): use all mutations
 
-    sample_list = samples_df[samples_df['participant_id'] == idx].index.tolist()
-    # restrict sample selection to only two samples at a time
-    sample_selection_corrected = [sample_list[0]] if sample_selection == [] else sample_selection[:2]
-
-    sigmas_val = 'Show CNV Sigmas' in sigmas
-
-    seg_df = []
-    for sample_id in sample_list:
-        this_seg_df = pd.read_csv(samples_df.loc[sample_id, 'cnv_seg_fn'], sep='\t')
-        if color == 'Differential':
-            this_seg_df['color_bottom'], this_seg_df['color_top'] = calc_color(this_seg_df, 'mu.major', 'mu.minor')
-        elif color == 'Cluster' and 'cluster_assignment' in this_seg_df.columns:
-            phylogic_color_dict = get_phylogic_color_scale()
-            this_seg_df['color_bottom'] = this_seg_df['cluster_assignment'].map(phylogic_color_dict)
-            this_seg_df['color_top'] = this_seg_df['color_bottom']
-        # unsure about clonal/subclonal
-        else:
-            this_seg_df['color_bottom'] = '#2C38A8'  # blue
-            this_seg_df['color_top'] = '#E6393F'  # red
-        this_seg_df['Sample_ID'] = sample_id
-        seg_df.append(this_seg_df)
-
-    maf_df['Sample_ID'] = maf_df[sample_id_col]
-    maf_df = maf_df[maf_df.Sample_ID.isin(sample_list)]
-    # maf_df = switch_contigs(maf_df)
-    # .replace giving: Series.replace cannot use dict-like to_replace and non-None value ??
-    maf_df['Chromosome'] = maf_df['Chromosome'].apply(lambda x: '23' if x == 'X' else '24' if x == 'Y' else x)
-
-    seg_trees = get_segment_interval_trees(pd.concat(seg_df))
-    maf_df = apply_segment_data_to_df(maf_df, seg_trees)
-
-    maf_df['Chromosome'] = maf_df['Chromosome'].astype(int)
-    if 'Cluster_Assignment' in list(maf_df):
-        maf_df['Cluster_Assignment'] = maf_df['Cluster_Assignment'].astype(int)
-        maf_df['cluster_color'] = maf_df['Cluster_Assignment'].apply(lambda x: cluster_color(x))
-
-    c_size_cumsum = np.cumsum([0] + list(csize.values()))
-    maf_df['x_loc'] = maf_df.apply(lambda x: c_size_cumsum[x['Chromosome'] - 1] + x['Start_position'], axis=1)
-    maf_df['VAF'] = maf_df['t_alt_count'] / (maf_df['t_alt_count'] + maf_df['t_ref_count'])
-
     for i, sample_id in enumerate(sample_selection_corrected):
-        this_maf_df = maf_df[maf_df[sample_id_col] == sample_id]
-        this_seg_df = seg_df[sample_list.index(sample_id)]
-
-        cnv_plot, start_trace, end_trace = pickle.load(open(f'{preprocess_data_dir}/cnv_figs/{sample_id}.cnv_fig.pkl', "rb"))
-        update_cnv_scatter_sigma_toggle(cnv_plot, sigmas_val)
-        update_cnv_scatter_color(cnv_plot, this_seg_df['color_bottom'], this_seg_df['color_top'], start_trace, end_trace)
-
-        if 'wxs_purity' in list(samples_df):
-            purity = float(samples_df.loc[sample_id, 'wxs_purity'])
-            ploidy = float(samples_df.loc[sample_id, 'wxs_ploidy'])
-            c_0, c_delta = calc_cn_levels(purity, ploidy)
-
-            if 'Display Absolute CN' in absolute:
-                this_maf_df['mu_major_adj'] = (this_maf_df['mu_major'] - c_0) / c_delta
-                this_maf_df['mu_minor_adj'] = (this_maf_df['mu_minor'] - c_0) / c_delta
-
-                mu_major_adj, mu_minor_adj, sigma_major_adj, sigma_minor_adj = calc_absolute_cn(
-                    this_seg_df['mu.major'], this_seg_df['mu.minor'], this_seg_df['sigma.major'], this_seg_df['sigma.minor'],
-                    c_0, c_delta
-                )
-
-                update_cnv_scatter_cn(
-                    cnv_plot,
-                    mu_major_adj,
-                    mu_minor_adj,
-                    sigma_major_adj,
-                    start_trace,
-                    end_trace
-                )
-            else:
-                this_maf_df['mu_major_adj'] = this_maf_df['mu_major']
-                this_maf_df['mu_minor_adj'] = this_maf_df['mu_minor']
-
-            this_maf_df['multiplicity_ccf'] = this_maf_df.apply(
-                lambda x: x.VAF * (purity * (x.mu_major_adj + x.mu_minor_adj) + 2 * (1 - purity)) / purity, axis=1
-            )
-
-             # calculate error bars for mutations
-            this_maf_df['error_top'] = this_maf_df.apply(
-                lambda x: calculate_error(x.t_alt_count, x.t_ref_count, purity, 0.975), axis=1)
-            this_maf_df['error_bottom'] = this_maf_df.apply(
-                lambda x: -1 * calculate_error(x.t_alt_count, x.t_ref_count, purity, 0.025), axis=1)
-
-            cnv_plot.add_trace(gen_mut_scatter(this_maf_df, sigmas_val, sample_id), row=i+1, col=1)
+        sample_maf_df = maf_df[maf_df['Sample_ID'] == sample_id]
+        cnv_subplots_fig.add_trace(gen_mut_scatter(sample_maf_df, sigmas_val, sample_id), row=i+1, col=1)
 
     return [
-        cnv_plot,
+        cnv_subplots_fig,
         sample_list,
         sample_selection_corrected
     ]
@@ -344,7 +336,7 @@ def gen_absolute_components(
     df = data.participant_df
     samples_df = data.sample_df
 
-    filtered_mutation_rows = None
+    filtered_mutation_rows = None  # todo why?
 
     cnv_plot, sample_list, sample_selection = gen_cnv_plot(df, idx, [], sigmas, color, absolute, selected_mutation_rows, filtered_mutation_rows, samples_df, preprocess_data_dir)
     button_clicks = None
