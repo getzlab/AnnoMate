7513f0980158756f73711ebb1ea562e4983c971b
Claudia Chu <cchu@broadinstitute.org>
2023-07-28 17:34:33 -0400

    update catalog formatting

--- example_notebooks/ReviewerCatalog.py
+++ example_notebooks/ReviewerCatalog.py
@@ -1,21 +1,23 @@
 import pandas as pd
 import requests
+import tqdm
+from IPython.display import display, HTML
 
 
 registered_reviewer_repos = [
-    'https://github.com/getzlab/JupyterReviewer',
-    'https://github.com/getzlab/PurityReviewers', 
+    'https://github.com/getzlab/AnnoMate',
+    'https://github.com/getzlab/PurityReviewer', 
     'https://github.com/getzlab/PatientReviewer',
     'https://github.com/getzlab/MutationReviewer',
     'https://github.com/getzlab/PhylogicReviewer'
 ]
 
-registry_fn = 'registry.tsv'
+catalog_fn = 'catalog.tsv'
 
-def gen_reviewer_registry():
+def gen_reviewer_catalog():
     repo_data_df = pd.DataFrame(columns=['Repo', 'Type', 'Name', 'Description', 'url'])
     i = 0
-    for repo_url in registered_reviewer_repos:
+    for repo_url in tqdm.tqdm(registered_reviewer_repos, total=len(registered_reviewer_repos)):
         repo_name = repo_url.rsplit('https://github.com/', 1)[-1]
         base_name = repo_name.rsplit('/', 1)[-1]
 
@@ -43,13 +45,43 @@ def gen_reviewer_registry():
                 repo_data_df.loc[i] = {'Repo': repo_name, 'Type': 'AppComponent', 'Name': filename, 'Description': '', 'url': url}
                 i += 1
     
-    repo_data_df.to_csv(registry_fn, sep='\t')
+    repo_data_df.to_csv(catalog_fn, sep='\t')
 
 def make_clickable(val):
     # target _blank to open new window
     return '<a target="_blank" href="{}">{}</a>'.format(val, val.rsplit('/')[-1])
 
 
-def display_registry_df():
-    repo_data_df = pd.read_csv(registry_fn, sep='\t', index_col=0).set_index(['Repo', 'Type', 'Name'])
-    return repo_data_df.style.format({'url': make_clickable, 'Description': lambda x: x if not pd.isna(x) else ''}) 
+def display_catalog_df(wrap_length=30):
+    catalog_data_df = pd.read_csv(catalog_fn, sep='\t', index_col=0)
+    catalog_data_df = catalog_data_df[
+        ~catalog_data_df['Name'].isin(['__init__', 'AppComponents', 'Reviewers', '__pycache__']) &
+        ~catalog_data_df['Name'].str.contains('cpython')
+    ]
+
+    catalog_data_df = catalog_data_df.set_index(['Repo', 'Type', 'Name'])
+    
+    s = catalog_data_df[['url', 'Description']].style.format(
+        {'url': make_clickable, 'Description': lambda x: x if not pd.isna(x) else ''}
+    ).set_properties(
+        **{'background-color': 'white'}
+    ).set_table_styles([
+        {'selector': 'th',
+        'props': [
+            # ('border-style', 'solid'),
+            # ('border-color', 'black'),
+            ('vertical-align','top')
+        ]
+        }
+    ])
+
+    for idx, group_df in catalog_data_df.groupby(level=0):
+        s.set_table_styles(
+            {group_df.index[0]: [{'selector': '', 'props': 'border-top: 1px solid black;'}]}, 
+            overwrite=False, 
+            axis=1
+        )
+
+    return s
+                       
+    
