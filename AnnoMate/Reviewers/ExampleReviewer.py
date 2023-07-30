8f963a78cd7689e42e9e9a12a04f6d55c43ad538
Claudia Chu <cchu@broadinstitute.org>
2023-07-30 00:16:28 -0400

    add extra lines to description

--- AnnoMate/Reviewers/ExampleReviewer.py
+++ AnnoMate/Reviewers/ExampleReviewer.py
@@ -1,5 +1,6 @@
 """Description
 A basic reviewer for the AnnoMate tutorial.
+Uses simulated data from simulated_data directory
 """
 from AnnoMate.Data import DataAnnotation
 from AnnoMate.DataTypes.GenericData import GenericData
