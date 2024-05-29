81c4ff8ed119497195fa74d48f0fe997ba3e13c2
cchu70 <cchu@broadinstitute.org>
2024-05-29 11:56:15 -0400

    Update test_example_reviewer.py

--- tests/test_example_reviewer.py
+++ tests/test_example_reviewer.py
@@ -2,16 +2,21 @@ import pandas as pd
 from AnnoMate.Reviewers.ExampleReviewer import ExampleReviewer
 
 def test_reviewer():
-    my_reviewer = ExampleReviewer()
-
-    # test state of the reviewer on instantiation
-    assert my_reviewer.get_annot_df() is None
-
-
     fn = '../tutorial_notebooks/example_data/AnnoMate_Tutorial/data_to_review_example.tsv'
     df = pd.read_csv(fn, sep='\t')
     df = df.set_index('sample_id')
-    df.head()
-
     assert df.shape[0] > 0
+
+    my_reviewer = ExampleReviewer()
+    output_pkl_path = './example_reviewer_data'
+    my_reviewer.set_review_data(data_path=output_pkl_path, 
+                                description='Intro to reviewers review session part 2',
+                                sample_df=df,
+                                preprocessing_str='Testing preprocessing')
+    
+    assert my_reviewer.review_data_interface.mh.metadata['freeze_data'] == False
+    # test state of the reviewer on instantiation
+    assert list(my_reviewer.list_data_attributes()) == ['index', 'description', 'annot_col_config_dict', 'annot_df', 'history_df', 'df']
+    assert my_reviewer.get_data_attribute('df').equals(df)
+    
     
