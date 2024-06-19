9203a30c63d13d74dc6cd34657ce76a9f0ea5737
Conor Messer <35932467+ConorMesser@users.noreply.github.com>
2024-06-18 22:18:26 -0700

    Update setup.py with new cnv_suite version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     packages=setuptools.find_packages(where=".", exclude=['zeption.examples', '']),
     python_requires=">=3.8", # last tested version: 3.9
     install_requires = ['setuptools<60.0', # https://numpy.org/doc/stable/reference/distutils_status_migration.html. Install before numpy
-                        'cnv-suite',
+                        'cnv-suite>=0.2.0',
                         'dash>=2.11.0',
                         'dash-bootstrap-components',
                         'dash-cytoscape',
